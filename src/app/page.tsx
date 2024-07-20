474de31dea9ba96c658ab6141ee3e03c34500aa5
Steven Krenn <stevekrenn@gmail.com>
2024-07-19 19:18:46 -0500

    added file substructure links to layout (head) component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
       Home Page
     </>
   );
